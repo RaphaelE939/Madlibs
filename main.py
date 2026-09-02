7ba3af0ef40e8b103346693331b551bcb1ab7bdc
C.R.E <raphaelE939@stemcollegiate.ca>
2026-09-02 15:38:10 -0600

    Code for Madlib
    I created a Madlib

---
+++ main.py
@@ -0,0 +1,22 @@
+plural_noun = input("Plural noun\n")
+adjective = input("Adjective\n")
+verb = input("Verb in past tense\n")
+name = input("Student name\n")
+number = input("Number\n")
+plural_noun2 = input("Second plural noun\n")
+adjective_2 = input("Another adjective ending with -ing\n")
+print(plural_noun)
+print(adjective)
+print(verb)
+print(name)
+print(number)
+print(plural_noun2)
+print(adjective_2)
+
+number = 3
+my_string = f"My number is: {number}."
+print(my_string)
+
+message = f" Early in the morning, {name}, awesomely walked down to see his/her {adjective} {plural_noun}. Suddenly, he/she saw {number} {plural_noun2} {adjective_2} in the sky. {name} {verb} away/to (from) them \""
+
+print(message)
