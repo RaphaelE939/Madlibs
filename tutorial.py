d0a8b638e6c5abbbdcb6f73b4877711fc43215f6
C.R.E <raphaelE939@stemcollegiate.ca>
2026-09-22 14:43:29 -0600

    Create tutorial.py

---
+++ tutorial.py
@@ -0,0 +1,19 @@
+noun = input("Noun\n")
+adjective = input("Adjective\n")
+verb = input("Verb ending in -ing\n")
+name = input("Student name\n")
+number = input("Number\n")
+plural_noun = input("Plural noun\n")
+adjective_2 = input("Another adjective\n")
+
+print(noun)
+print(adjective)
+print(verb)
+print(name)
+print(number)
+print(plural_noun)
+print(adjective_2)
+
+message = f"Today in computer science class, Mr. Forsyth taught us how to program a(n) {noun}. First, we clicked the {adjective} button, then the computer started {verb}. Suddenly, {name} accidentally created {number} {plural_noun}! Mr. Forsyth said, \"This is the most {adjective_2} bug I've ever seen.\""
+
+print(message)
